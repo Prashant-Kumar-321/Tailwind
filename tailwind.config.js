7999dcac26a4f7a716f0de34b3d40df2cfc471c9
Prashant-Kumar-321 <pk6176838@gmail.com>
2024-05-16 15:00:56 +0530

    Design navigation bar hero section and student section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-theme': '#240750',
+      }
+    },
   },
   plugins: [],
 }
