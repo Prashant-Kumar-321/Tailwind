946cd8ebd9ce2d47567cd73f5d7f27961c705f8f
Prashant-Kumar-321 <pk6176838@gmail.com>
2024-05-26 07:16:52 +0530

    implement signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     extend: {
       colors: {
         'primary-theme': '#240750',
-      }
+        'new-theme': '#030637', 
+      }, 
+      gridTemplateColumns: {
+        '3fr-2fr': '3fr  2fr'
+      },
     },
   },
   plugins: [],
